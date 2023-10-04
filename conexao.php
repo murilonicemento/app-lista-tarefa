3773a97e8a7f27b0e1ca36ca01911e1863f79a9e
murilonicemento <murilo_nascimento22@outlook.com>
2023-10-04 17:09:31 -0300

    invisibilização de credenciais privadas

--- conexao.php
+++ conexao.php
@@ -1,9 +1,9 @@
 <?php
 
 class Conexao {
-  private $host = "localhost";
-  private $dbname = "php_com_pdo";
-  private $user = "root";
+  private $host = "";
+  private $dbname = "";
+  private $user = "";
   private $pass = "";
 
   public function conectar() {
